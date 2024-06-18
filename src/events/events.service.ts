86bbc777f95956fded93c6258c829eaf49af159b
Eduardo <eduardomuchak@gmail.com>
2024-06-17 22:35:11 -0300

    feat(wip): :sparkles: creates the logic for the events service to create a new event

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { DB_PROVIDER, DB } from '@/db/database.providers';
+import { events } from '@/db/schema';
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+  constructor(@Inject(DB_PROVIDER) private readonly db: DB) {}
+
+  async create(createEventDto: CreateEventDto) {
+    const result = await this.db
+      .insert(events)
+      .values({
+        name: createEventDto.name,
+        description: createEventDto.description,
+        date: createEventDto.date,
+        price: createEventDto.price,
+      })
+      .returning();
+    return result[0];
   }
 
   findAll() {
