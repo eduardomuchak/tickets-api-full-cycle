586c2a83b310873fec8d11c126a22ea251345f85
Eduardo <eduardomuchak@gmail.com>
2024-06-19 20:09:20 -0300

    feat: :sparkles: adiciona connectWithRetry

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -6,6 +6,27 @@ import { Pool, PoolConfig } from 'pg';
 const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_HOST, DATABASE_PORT } =
   process.env;
 
+const connectWithRetry = async (
+  options: PoolConfig,
+  retries = 5,
+  delay = 5000,
+) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      const pool = new Pool(options);
+      await pool.query('SELECT 1');
+      return pool;
+    } catch (err) {
+      console.error(
+        `Attempt ${i + 1} to connect to database failed. Retrying in ${delay / 1000} seconds...`,
+        err,
+      );
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+  throw new Error('Failed to connect to the database after multiple attempts');
+};
+
 const migrate = async () => {
   console.log('===== Starting Migration =====');
 
@@ -17,7 +38,8 @@ const migrate = async () => {
     port: parseInt(DATABASE_PORT, 10) || 5432,
   };
 
-  const db: NodePgDatabase<Record<string, never>> = drizzle(new Pool(options));
+  const pool = await connectWithRetry(options);
+  const db: NodePgDatabase<Record<string, never>> = drizzle(pool);
 
   await dbMigrate(db, { migrationsFolder: path.join(__dirname, 'migrations') });
 
