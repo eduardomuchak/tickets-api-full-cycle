8243a00196be901a9f631403d2ed0d0fd4af978c
Eduardo <eduardomuchak@gmail.com>
2024-06-19 20:19:44 -0300

    fix: :bug: remove funcao de retry connection

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -6,27 +6,6 @@ import { Pool, PoolConfig } from 'pg';
 const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_HOST, DATABASE_PORT } =
   process.env;
 
-const connectWithRetry = async (
-  options: PoolConfig,
-  retries = 5,
-  delay = 5000,
-) => {
-  for (let i = 0; i < retries; i++) {
-    try {
-      const pool = new Pool(options);
-      await pool.query('SELECT 1');
-      return pool;
-    } catch (err) {
-      console.error(
-        `Attempt ${i + 1} to connect to database failed. Retrying in ${delay / 1000} seconds...`,
-        err,
-      );
-      await new Promise((res) => setTimeout(res, delay));
-    }
-  }
-  throw new Error('Failed to connect to the database after multiple attempts');
-};
-
 const migrate = async () => {
   console.log('===== Starting Migration =====');
 
@@ -38,9 +17,7 @@ const migrate = async () => {
     port: parseInt(DATABASE_PORT, 10) || 5432,
   };
 
-  const pool = await connectWithRetry(options);
-  const db: NodePgDatabase<Record<string, never>> = drizzle(pool);
-
+  const db: NodePgDatabase<Record<string, never>> = drizzle(new Pool(options));
   await dbMigrate(db, { migrationsFolder: path.join(__dirname, 'migrations') });
 
   console.log('===== Completed Migration =====');
